e8b52acb0e68656b5cc75cb9cb736f4731870166
cgonzalez816 <Cgonzalez816@gmail.com>
2016-03-20 21:04:50 -0600

    Nuevo

---
+++ Gato/src/gato/Gato.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gato;
+
+import Usuarios.Usuarios;
+
+/**
+ *
+ * @author treznor
+ */
+public class Gato {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Usuarios U = new Usuarios();
+        U.setVisible(true);
+        
+    }
+    
+}
